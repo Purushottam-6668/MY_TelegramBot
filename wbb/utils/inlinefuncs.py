ab7b06690a48e87e66319d2d3349ee668d13e4cb
TheProgrammerCat <sangramghangale@yahoo.com>
2021-09-27 15:07:25 +0530

    Update inlinefuncs.py

--- wbb/utils/inlinefuncs.py
+++ wbb/utils/inlinefuncs.py
@@ -407,8 +407,8 @@ async def tg_search_func(answers, text, user_id):
 
 
 async def music_inline_func(answers, query):
-    chat_id = -1001445180719
-    group_invite = "https://t.me/joinchat/vSDE2DuGK4Y4Nzll"
+    chat_id = -1001267448966
+    group_invite = "https://t.me/Friends_Group_Chatting"
     try:
         messages = [
             m
